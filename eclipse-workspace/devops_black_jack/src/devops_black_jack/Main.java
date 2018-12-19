a587c0926c0bc44dfbac567f7bfae2912dcce65f
nothrazim <kevincederqvist@gmail.com>
2018-12-19 10:56:01 +0100

    lots of tests

--- eclipse-workspace/devops_black_jack/src/devops_black_jack/Main.java
+++ eclipse-workspace/devops_black_jack/src/devops_black_jack/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello weldt!");
+		System.out.println("Hello weldt!");	
 		System.out.println("FISK");
+		System.out.println("THIS IS A TEST");
+		System.out.println("THIS IS A LOT OF TESTS");
+		System.out.println("THIS IS A TEST");
+		System.out.println("THIS IS A TEST");
 		System.out.println("THIS IS A TEST");
 	}
 }
