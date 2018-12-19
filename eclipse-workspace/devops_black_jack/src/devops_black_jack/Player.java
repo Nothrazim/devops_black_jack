b7e80490bb2a080e362c41f9e48eccd17cc78820
Thomas Touma <Thomas.touma@hotmail.com>
2018-12-19 13:55:03 +0100

    Updated the player class

--- eclipse-workspace/devops_black_jack/src/devops_black_jack/Player.java
+++ eclipse-workspace/devops_black_jack/src/devops_black_jack/Player.java
@@ -1,5 +1,19 @@
 package devops_black_jack;
 
 public class Player {
-
+	int pott;
+	int balance;
+	
+	
+	
+	public Player(String name, int balance){
+		System.out.println("Welcome " + name + ", you have " + balance +" to play for");
+	}
+	
+	
+	
+	public int checkBalance() {
+		
+		return balance;
+	}
 }
