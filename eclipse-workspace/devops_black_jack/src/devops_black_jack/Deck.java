0fbf9fb5e3273b223c8a57fa1ad57e04bb41126e
nothrazim <kevincederqvist@gmail.com>
2018-12-19 14:15:14 +0100

    Removed superfluous prints, added call to loop_cards (needs renaming)

--- eclipse-workspace/devops_black_jack/src/devops_black_jack/Deck.java
+++ eclipse-workspace/devops_black_jack/src/devops_black_jack/Deck.java
@@ -11,12 +11,23 @@ public class Deck {
 
 	String[] suites = {"Hearts", "Spades", "Diamonds", "Clubs"};
 	
-	public void loop_cards(){
+	void loop_cards(){
 		for(int suit = 0; suit < suites.length; suit++) {
 			System.out.println("Suit: " + suites[suit]);
-			for(int a = 1; a < 13; a++) {
-				deck[a] = new Card(a + " of " + suites[suit], a);
-				System.out.println(deck[a].name);
+			for(int n = 1; n < 14; n++) {
+				if (n == 11) {
+					deck[n] = new Card("Jack of " + suites[suit], 10);
+					}
+				else if (n == 12) {
+					deck[n] = new Card("Queen of " + suites[suit], 10);
+					}
+				else if (n == 13) {
+					deck[n] = new Card("King of " + suites[suit], 10);
+					}
+				else {
+					deck[n] = new Card(n + " of " + suites[suit], n);
+					System.out.println(deck[n].name);
+					}
 				}
 			}
 		}
--- eclipse-workspace/devops_black_jack/src/devops_black_jack/Main.java
+++ eclipse-workspace/devops_black_jack/src/devops_black_jack/Main.java
@@ -6,21 +6,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello weldt!");	
 
-		System.out.println("FISK");
-		System.out.println("THIS IS A TEST");
+		Deck Decktest = new Deck();
+		Decktest.loop_cards();
 
-		System.out.println("The master FISK 2");
-
-		System.out.println("THIS IS A LOT OF TESTS");
-		System.out.println("THIS IS A TEST");
-		System.out.println("THIS IS A TEST");
-		System.out.println("THIS IS A TEST");
-
-
-		for(int i = 0; i < 100; i++) {
-			System.out.println("hehe nr hehe: " + i);
-			
-			
-		}
+		//for(int i = 0; i < 100; i++) {
+			//System.out.println("hehe nr hehe: " + i);
+			//}
 	}
 }
